3337180b9c910af00a10d0c20dabbb862303196a
JoaquinDev <joaquin@isla.io>
2014-02-10 07:55:37 +0100

    Add models and empty server functions
    Add models and empty server functions
    
    Contact: joaquin@isla.io

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 var util = require('util');
+
+var notFound = function(res, id){}
+
+var notValid = function(res, next, err){}
+
+var Resource = function(Model, options){}
+
+Resource.prototype.query = function(options){}
+
+Resource.prototype.detail = function(options){}
+
+Resource.prototype.insert = function(){}
+
+Resource.prototype.update = function(){}
+
+Resource.prototype.remove = function(){}
+
+Resource.prototype.serve = function(path, server){}
+
+
---
+++ makefile
@@ -0,0 +1,7 @@
+#CWD	=	$(shell pwd)
+
+server:
+	node api/index.js 
+
+#test: 
+#	mocha --reporter spec test/**/*.spec.js

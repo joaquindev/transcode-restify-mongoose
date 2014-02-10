96a1dea6d3edbab269579dc3662f138e647b5d6f
JoaquinDev <joaquin@isla.io>
2014-02-10 08:32:55 +0100

    Add MONGOLAB_URI string connection
    Add MONGOLAB_URI string connection
    
    Contact: joaquin@isla.io

--- api/web.js
+++ api/web.js
@@ -4,7 +4,16 @@ var restifyMongoose = require('../index.js');
 var models = require('./models');
 
 var theport = process.env.PORT || 5000;
-mongoose.connect('mongodb://localhost/restify-mongoose-examples');
+var uristring = process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL || 
+    'mongodb://localhost/restify-mongoose-example-api';
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 
 var server = restify.createServer({
     name: 'restify.mongoose.examples.notes',
